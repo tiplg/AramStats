d8c1643cc055180f88ef8c2bd4a493a3bb5ed350
Tijmen De Jong <tijmen.dejong@hotmail.com>
2017-07-03 01:28:41 +0200

    add player

---
+++ GameFinder/GameFinder/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFinder
+{
+    class Player
+    {
+        public Player(long summonerID)
+        {
+            SummonerID = summonerID;
+        }
+
+        public long SummonerID { get; set; }
+        public long AccountID { get; set; }
+        public long TotalMatchesChecked { get; set; }
+        public long AramsFound { get; set; }
+        public long CheckedUntill { get; set; }
+
+
+    }
+}
